f712070ae185c3ba00a5459a310311079082ced2
ITLAB1 <ITLAB1@DESKTOP-B51HF4U>
2023-05-09 07:02:54 +0530

    Add project files.

---
+++ MethodOverloading OOP/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MethodOverloading_OOP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_method1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_method2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_method3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_method4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_method5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_method6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MethodOverloading OOP/methodoverloading.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading_OOP
+{
+    public class methodoverloading
+    {
+        public int AddNumbers(string s1,string s2)
+        {
+            int n1 = Convert.ToInt32(s1);
+            int n2 = Convert.ToInt32(s2);
+            return n1 + n2;
+        }
+        public int AddNumbers(string s1, string s2,string s3)
+        {
+            int n1 = Convert.ToInt32(s1);
+            int n2 = Convert.ToInt32(s2);
+            int n3 = Convert.ToInt32(s3);
+            return n1 + n2 + n3;
+        }
+    }
+}
